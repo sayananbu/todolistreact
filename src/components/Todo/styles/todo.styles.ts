79c6f33d4e33170dbdeb9a3448f2a3d7ade8efcf
sayananbu <67857676+sayananbu@users.noreply.github.com>
2023-06-13 17:33:05 +0300

    Update todo.styles.ts

--- src/components/Todo/styles/todo.styles.ts
+++ src/components/Todo/styles/todo.styles.ts
@@ -13,7 +13,8 @@ export const STodoContainer = styled.div`
     height: 50px;
     border: 1px solid #ccc;
     border-radius: 8px;
-    margin: 15px auto 0 auto;
+    //margin: 15px 5px 0 5px;
+	margin: 0;
     box-sizing: border-box;
 	@media ${device.mobileL}{
 		height: 80px;
